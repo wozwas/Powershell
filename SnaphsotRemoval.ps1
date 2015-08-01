44a55de869c8650cb5da115014e4356e18145dbf
Jesse Johnson <Jesse@brightworksgroup.net>
2015-08-01 02:05:05 -0400

    Remove vSphere Snapshots
    Removes any snapshots containing the word "Daily" in their name, older
    than 1 day.

---
+++ SnaphsotRemoval.ps1
@@ -0,0 +1,10 @@
+﻿Enter-PSSession localhost
+add-pssnapin VM*
+Connect-VIServer -Server localhost
+$VMlist = @("Calypso.callard.local", "Rancid", "Uranus.callard.local", "Titan.callard.local", "Cressida.callard.local")
+foreach ($objItem in $VMlist) {
+Get-VM $objItem |
+Get-Snapshot |
+Where-Object {$_.Name.Contains("Daily") -and $_.Created -lt (Get-Date).AddDays(-1) } |
+Remove-Snapshot -Confirm:$false
+}
